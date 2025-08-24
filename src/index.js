8897b21467a6e0e4af0b67833872ed28923a2f77
Olga <minaeva9@gmail.com>
2025-08-24 12:40:32 -0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || matrix.length === 0) return [];
+
+  const sortedArr = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      const jIndex = i % 2 === 0 ? j : matrix[i].length - j - 1;
+      sortedArr.push(matrix[i][jIndex]);
+    }
+  }
+  return sortedArr;
 };
